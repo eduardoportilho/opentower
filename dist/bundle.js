e710d8a51aa1ab3a0c8941fbc2d2a4210d75ea07
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-07-25 10:25:25 -0300

    Grid class

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,9 +65,68 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
 
 
+var _grid = __webpack_require__(1);
+
+var _grid2 = _interopRequireDefault(_grid);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var canvas = document.getElementById('canvas');
+var gridDimensions = {
+  width: 600,
+  heigth: 600,
+  cellSize: 50
+};
+var grid = new _grid2.default(canvas, gridDimensions);
+grid.draw();
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Grid = function () {
+  function Grid(canvas, dimensions) {
+    _classCallCheck(this, Grid);
+
+    this.canvas = canvas;
+    this.dimensions = dimensions;
+  }
+
+  _createClass(Grid, [{
+    key: "draw",
+    value: function draw() {
+      console.log("draw");
+    }
+  }]);
+
+  return Grid;
+}();
+
+exports.default = Grid;
+
+// expose draw method ✔︎
+// draw grid
+// draw object's paths (or call object draw)
+// - towers
+// - goons
+// - bullets?
+// receive user click and call game methods
 
 /***/ })
 /******/ ]);
---
+++ src/grid.js
@@ -0,0 +1,19 @@
+// ✔︎ - expose draw method
+// draw grid
+// draw object's paths (or call object draw)
+// - towers
+// - goons
+// - bullets?
+// receive user click and call game methods
+
+class Grid {
+  constructor (canvas, dimensions) {
+    this.canvas = canvas
+    this.dimensions = dimensions
+  }
+
+  draw () {
+  }
+}
+
+export default Grid
