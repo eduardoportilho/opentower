22c108ac8dc6f75b1166b46178d151e74c5857a6
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-09-10 20:05:35 +0200

    Store path point in cell

--- src/iso/cell.js
+++ src/iso/cell.js
@@ -1,10 +1,11 @@
 export default class Cell {
-  constructor ({row, col, tile, object, spawn = false, target = false}) {
+  constructor ({row, col, tile, object, spawn, target, pathPoints}) {
     this.row = row
     this.col = col
     this.tile = tile
     this.object = object
     this.spawn = spawn
     this.target = target
+    this.pathPoints = pathPoints
   }
 }
